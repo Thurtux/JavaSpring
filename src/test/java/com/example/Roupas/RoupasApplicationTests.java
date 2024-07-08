4562a35de519930f0fc0033de18c3f5eca3e8ccf
Thurtux <arthursantoslopes91@gmail.com>
2024-07-08 14:44:19 -0300

    First Commit

---
+++ src/test/java/com/example/Roupas/RoupasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Roupas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoupasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
